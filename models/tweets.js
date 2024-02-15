86f088fe0466cc1f35c66b6f4c589d09f5a4cc39
Noé <n.mazzu@hotmail.fr>
2024-02-15 10:47:26 +0100

    model tweets

---
+++ models/tweets.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const tweetSchema = mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  text: String,
+  date: Date,
+});
+
+const Tweet = mongoose.model("tweets", tweetSchema);
+
+module.exports = Tweet;
